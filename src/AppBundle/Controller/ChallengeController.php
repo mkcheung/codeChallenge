b463054256c03a11e9cdf9d8157a35f5e9131587
Mars Cheung <mars.kwong.cheung@gmail.com>
2017-04-11 12:53:52 -0700

    replace address_form with form builder - remove constants from config.yml and extract from database instead, placing within the form itself

--- src/AppBundle/Controller/ChallengeController.php
+++ src/AppBundle/Controller/ChallengeController.php
@@ -2,53 +2,53 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\MeetingType;
+use AppBundle\Entity\Region;
+use AppBundle\Form\MeetingFilterType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class ChallengeController extends Controller {
+class ChallengeController extends Controller
+{
 
-    const CACHE_EXPIRATION_TIME = 3600;
+    /**
+     * @Route("/challenge/")
+     * @codeCoverageIgnore
+     */
+    public function indexAction()
+    {
 
-	/**
-	* @Route("/challenge/")
-    * @codeCoverageIgnore
-	*/
-	public function indexAction()
-	{
+        $meetingTypeRepo = $this->getDoctrine()->getRepository(MeetingType::class);
+        $regionRepo = $this->getDoctrine()->getRepository(Region::class);
 
-        return $this->render('default/address_input.html.twig', array(
-            'page_title' => 'Request Meeting Times, Types and Locations',
-            'states' => $this->container->getParameter('states'),
+        $allMeetingTypes = $meetingTypeRepo->findAll();
+        $allRegions = $regionRepo->findAll();
 
+        $form = $this->createForm(MeetingFilterType::class, [$allMeetingTypes, $allRegions], array(
+            'action' => $this->generateUrl('/challenge/meetingsFromLocation'),
+            'method' => 'POST'
+        ))->createView();
 
+        return $this->render('default/address_input.html.twig', [
+            'page_title'    => 'Request Meeting Times, Types and Locations',
+            'form'          => $form
+        ]);
+    }
 
-
-
-
-
-
-            'days' => $this->container->getParameter('days'),
-            'meeting_types' => $this->container->getParameter('meeting_types')
-        ));
-	}
-
-	/**
-	* @Route("/challenge/meetingsFromLocation")
-	*/
-	public function meetingsFromLocationAction(Request $request)
-	{
-
+    /**
+     * @Route("/challenge/meetingsFromLocation", name="/challenge/meetingsFromLocation")
+     */
+    public function meetingsFromLocationAction(Request $request)
+    {
         $treatmentCenterService = $this->get('app.treatment_center_service');
-        $meetingInformation = $treatmentCenterService->getTreatmentCenters($request);
+        $meetingInformation     = $treatmentCenterService->getTreatmentCenters($request);
 
-     	$response = $this->render('default/meeting.html.twig', array(
+        $response = $this->render('default/meeting.html.twig', [
             'meetingInformation' => $meetingInformation,
-        ));
-
-//        $response->setSharedMaxAge(static::CACHE_EXPIRATION_TIME);
+        ]);
 
         return $response;
-	}
+    }
 }
